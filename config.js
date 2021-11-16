67fae990d652d0ae4507c9a825431f506f108f59
DinhPhucDong <94447903+DinhPhucDong@users.noreply.github.com>
2021-11-16 17:43:59 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
+    titleWeb: "Đinh Phúc Đông và Em",
+    introTitle: 'For U',
+    introDesc: `Anh tên là Đinh Phúc Đông 
+    Quê anh ở Thái Bình và anh 2009
+    Anh chỉ muốn nói với em một câu thôi
+    Anh iu em nhìu lắm`,
+    btnIntro: 'Ấn zô đây ik',
+    title: 'Ấn vào là phải iu đó ',
+    desc: 'Iu mình hok dza ',
+    btnYes: 'Iu lắm ak <33',
+    btnNo: 'Không nha ( Không ấn vào được đừng cố ):3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
+    btnReply: 'Gửi bạn <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    mess: 'Mình biết mà. Mình cũng iu b lắm ak',
+    messDesc: 'Nhắn tin cho mình đi rùi mình iu nhau.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://m.me/DinhPhucDong.xD' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
