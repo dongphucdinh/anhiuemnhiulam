347575f1301176bac71e2d595dfa3191813d089e
Đinh Phúc Đông <94447903+DinhPhucDong@users.noreply.github.com>
2021-12-01 15:09:02 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,19 @@
 const CONFIG = {
-    titleWeb: "Đinh Phúc Đông và Em",
-    introTitle: 'For U',
-    introDesc: `Mình tên là Đinh Phúc Đông 
-    quê mình ở Thái Bình và 
+    titleWeb: "Đinh Phúc Đông tặng cậu",
+    introTitle: 'Đọc đi nhé',
+    introDesc: `Mình thấy bạn rất cute dễ thương 
+    xinh đẹp hiền lành nết na  
     mình chỉ muốn nói với cậu một câu thôi
     là... Là gì thì ấn xuống dưới nhé`,
     btnIntro: 'Ấn vô đây nè',
     title: 'Mình thích cậu lắm á',
     desc: 'Cậu có thích tớ không',
     btnYes: 'Tớ cũng thích cậu ><',
-    btnNo: 'Không nha ( Không ấn vào được đừng cố )',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại thích mình <3',
+    btnNo: 'Không nha',
+    question: 'Tại sao bạn lại thích mình vậy???',
     btnReply: 'Send',
-    reply: 'Tại vì mình thích cậu ák >3',
-    mess: 'Mình biết mà. Mình cũng thích cậu lắm ak',
+    reply: 'Vì bạn cute dễ thương á',
+    mess: 'Mình biết mà. Mình cũng thích cậu lắm',
     messDesc: 'Nhắn tin cho mình đi rùi mình iu nhau.',
     btnAccept: 'Ấn vô đây nè',
     messLink: 'https://m.me/DinhPhucDong.xD' //link mess của các bạn. VD: https://m.me/nam.nodemy
